a2996f4aa4ca80cfc7eedc863efa32e8e38b155d
Neilgle <neilgle.new@gmail.com>
2015-02-10 13:42:32 +0800

    front page style

---
+++ public/js/util.js
@@ -0,0 +1,17 @@
+define(function() {
+  var clsname = '.x-mask';
+
+  return {
+    loading: function(fn){
+      fn && fn();
+      $(clsname)
+      $(clsname + '-loading').css('display','block');
+    },
+    loadingEnd: function(stop, fn){
+      $(clsname + '-loading').css('display','none');
+      $(clsname).css('background','rgba(0,0,0,.5)');
+      !stop && $(clsname).css('display','none');
+      fn && fn();
+    }
+  }
+});
