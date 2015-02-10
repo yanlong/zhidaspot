f975970665d25993dda17eec20cad3a03253860c
yanlong <yanlong@baidu.com>
2015-02-10 19:33:25 +0800

    Add product model

--- library/models.js
+++ library/models.js
@@ -4,12 +4,21 @@ var Schema = mongoose.Schema;
 var NewsSchema = Schema({
     title: String,
 })
+
+var ProductSchema = Schema({
+    name: String,
+})
+
 var AppSchema = Schema({
     id: Number,
     name: String,
     news:[{
         type: Schema.Types.ObjectId,
         ref: 'News'
+    }],
+    products:[{
+        type: Schema.Types.ObjectId,
+        ref: 'Product',
     }]
 })
 
@@ -21,4 +30,5 @@ AppSchema.methods.speak = function () {
 module.exports = {
     App: mongoose.model('App', AppSchema),
     News: mongoose.model('News', NewsSchema),
+    Product: mongoose.model('Product', ProductSchema),
 }
