d7d4f42f8c42b28aac4e2c252c2ff530ae8c4016
yanlong <yanlong@baidu.com>
2015-02-10 18:18:17 +0800

    Add App/News and their relationship

--- library/models.js
+++ library/models.js
@@ -1,13 +1,24 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-var AppSchema = mongoose.Schema({
+var NewsSchema = Schema({
+    title: String,
+})
+var AppSchema = Schema({
     id: Number,
     name: String,
+    news:[{
+        type: Schema.Types.ObjectId,
+        ref: 'News'
+    }]
 })
+
+
 
 AppSchema.methods.speak = function () {
     console.log(this.name)
 }
 module.exports = {
     App: mongoose.model('App', AppSchema),
+    News: mongoose.model('News', NewsSchema),
 }
--- library/mongo.js
+++ library/mongo.js
@@ -6,31 +6,29 @@ db.on('error', console.log.bind(console, 'connection error:'));
 db.once('open', function(callback) {
     // yay!
 });
-console.log('ffff')
-
-var kitty = new models.App({
-    name: 'Zildjian',
-    // age: 1,
-});
-kitty.speak();
 mongoose.connect('mongodb://10.48.222.106:8301/xxx');
+console.log('ffff')
+var news1 = new models.News({
+    title: 'fuck',
+})
 
-kitty.save(function(err) {
-    if (err) // ...
-        console.log('meow');
-    console.log('done')
-});
+news1.save(function(err) {
+    var kitty = new models.App({
+        name: 'Zildjian',
+        news: [news1._id]
+    });
 
-models.App.find({
-    name: 'Zildjian'
-}, {
-    name: 1,
-    age:1,
-    _id: 1
-}, {
-    // limit: 10,
-    sort: 'age'
-}, function(err, cats) {
-    console.log(cats)
-    console.log(cats.length)
+
+    kitty.save(function(err) {
+        if (err) // ...
+            console.log(err);
+        else
+            console.log('done')
+        models.App.find({
+            name: 'Zildjian'
+        }).populate('news').exec(function(err, cats) {
+            console.log(cats)
+            console.log(cats.length)
+        })
+    });
 })
