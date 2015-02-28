1be4b1795d6ae99b22352e763d86576d3a220935
yanlong <yanlong@baidu.com>
2015-02-28 18:01:10 +0800

    Add zhidaspot as global variable

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: req.data && req.data.company.name });
+    res.render('index', {
+        title: req.data && req.data.company.name,
+        data: JSON.stringify(req.data|| {}),
+    });
 });
-
 module.exports = router;
