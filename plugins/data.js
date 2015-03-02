7df2e51c2ce1da6d1a5ce557d4caeab865aa04b2
yanlong <yanlong@baidu.com>
2015-03-02 15:47:44 +0800

    Fix app/data bug; products page done

--- plugins/data.js
+++ plugins/data.js
@@ -1,7 +1,7 @@
 var Models = require('../library/models.js');
 module.exports = function (req, res, next) {
     if (req.query.appid) {
-        Models.App.findById(req.query.appid).populate('news products promotion contact attracting company').exec(function (err, doc) {
+        Models.App.findById(req.query.appid).populate('news product promotion contact attracting company').exec(function (err, doc) {
             if (err) return next(err);
             req.data = doc;
             next();
